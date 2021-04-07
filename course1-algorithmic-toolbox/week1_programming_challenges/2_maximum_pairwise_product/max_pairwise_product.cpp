1e90782f4a11856ada660c71c2ec34fc45e8093f
ChangYuan Liiu <changyuan@ustc.edu>
2021-04-06 20:10:24 -0400

    Completed Course 01 Week 01
    Corrected gcc compiler setup

--- course1-algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ course1-algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,24 +2,26 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+uint64_t MaxPairwiseProduct(const std::vector<uint64_t>& numbers) {
+    uint64_t max_product = 0;
     int n = numbers.size();
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
-        }
-    }
-
+    // for (int first = 0; first < n; ++first) {
+    //     for (int second = first + 1; second < n; ++second) {
+    //         max_product = std::max(max_product,
+    //             numbers[first] * numbers[second]);
+    //     }
+    // }
+    std::vector<uint64_t> nums = numbers;
+    std::sort(nums.begin(), nums.end());
+    max_product = nums[n-2] * nums[n-1];
     return max_product;
 }
 
 int main() {
     int n;
     std::cin >> n;
-    std::vector<int> numbers(n);
+    std::vector<uint64_t> numbers(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
