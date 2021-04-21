50c0d6fff48cb403904974f077d206a1de7f968f
ChangYuan Liiu <changyuan@ustc.edu>
2021-04-20 23:27:44 -0400

    Course 01 Week 04, 2/6

--- course1-algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ course1-algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -5,8 +5,21 @@
 using std::vector;
 
 int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
+  int left = 0, right = (int)a.size()-1; 
   //write your code here
+  while(right >= left){
+    int mid = (int) (left+right)/2;
+    if(a[mid] == x){
+      return mid;
+    }
+    else if(a[mid] < x){
+      left = mid+1;
+    }
+    else{
+      right = mid-1;
+    }    
+  }
+  return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
@@ -31,6 +44,6 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+    std::cout << binary_search(a, b[i]) << ' ';
   }
 }
--- course1-algorithmic-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ course1-algorithmic-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -4,17 +4,49 @@
 
 using std::vector;
 
-int get_majority_element(vector<int> &a, int left, int right) {
+int64_t get_majority_element(vector<int64_t> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  int mid = (int) (left + right)/2;
+  int64_t left_majority = get_majority_element(a, left, mid);
+  int64_t right_majority = get_majority_element(a, mid, right);
+  if(left_majority  == right_majority){
+    return left_majority;
+  }
+  else{
+    int left_count = 0;    
+    if(left_majority != -1){
+      for(int i = left; i < right; ++i){
+        if(a[i] == left_majority){
+          left_count++;
+        }
+      }
+    }
+
+    int right_count = 0;
+    if(right_majority != -1){
+      for(int i = left; i < right; ++i){
+        if(a[i] == right_majority){
+          right_count++;
+        }
+      }
+    }
+
+    if(left_count > (int) (right-left)/2){
+      return left_majority;
+    }
+    else if(right_count > (int) (right-left)/2){
+      return right_majority;
+    }
+  }
   return -1;
 }
 
 int main() {
   int n;
   std::cin >> n;
-  vector<int> a(n);
+  vector<int64_t> a(n);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
