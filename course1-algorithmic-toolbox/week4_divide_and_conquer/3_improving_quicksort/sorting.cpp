6fd6fc0a738d4eeb737179f731b7c25eefbf413b
ChangYuan Liiu <changyuan@ustc.edu>
2021-04-21 21:40:58 -0400

    Course 01 Week 04, passed 3/6

--- course1-algorithmic-toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ course1-algorithmic-toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -5,17 +5,44 @@
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
+// int partition2(vector<int> &a, int l, int r) {
+//   int x = a[l];
+//   int j = l;
+//   for (int i = l + 1; i <= r; i++) {
+//     if (a[i] <= x) {
+//       j++;
+//       swap(a[i], a[j]);
+//     }
+//   }
+//   swap(a[l], a[j]);
+//   return j;
+// }
+
+vector<int> partition3(vector<int> &a, int l, int r) {
+  vector<int> result;
   int x = a[l];
-  int j = l;
+  int j = l + 1;
+  int k = l + 1;
+
   for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
+    if (a[i] < x) {
       swap(a[i], a[j]);
+      j++;
+    }
+    else if(a[i] == x){
+      swap(a[i], a[j]);
+      swap(a[j], a[k]);
+      j++; 
+      k++;
     }
   }
-  swap(a[l], a[j]);
-  return j;
+  int m = std::min(k-l, j-k);
+  for(int i = l; i < l+m; ++i){
+    swap(a[i], a[j-i+l-1]);
+  }
+  result.push_back(j-k+l);
+  result.push_back(j-1);
+  return result;
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
@@ -25,10 +52,11 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  vector<int> m;
+  m = partition3(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, m[0] - 1);
+  randomized_quick_sort(a, m[1] + 1, r);
 }
 
 int main() {
