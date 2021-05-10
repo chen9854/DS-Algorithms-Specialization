e8da05452876e4ba071c337e5d9b832f50fe0ca3
ChangYuan Liiu <changyuan@ustc.edu>
2021-05-10 10:56:42 -0400

    Course 01 Week 05, 2/5

--- course1-algorithmic-toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ course1-algorithmic-toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -1,20 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <map>
+#include <limits>
+#include <set>
 using std::vector;
-
+// vector<int> optimal_sequence(int n) {
+//   std::vector<int> sequence;
+//   while (n >= 1) {
+//     sequence.push_back(n);
+//     if (n % 3 == 0) {
+//       n /= 3;
+//     } else if (n % 2 == 0) {
+//       n /= 2;
+//     } else {
+//       n = n - 1;
+//     }
+//   }
+//   reverse(sequence.begin(), sequence.end());
+//   return sequence;
+// }
 vector<int> optimal_sequence(int n) {
+  std::map<int,int> min_num_opts;
+  std::vector<int> prev;
   std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+  // std::set<int> coins = {1,3,4};
+  min_num_opts[0] = -1;
+  prev.push_back(0);
+  for(int im = 1; im <= n; ++im)  {
+    min_num_opts[im] = std::numeric_limits<int>::max();
+    prev.push_back(0);
+    std::set<int> coins = {im-1, im/2, im/3};
+    if(im % 3 == 0){
+      int num_coins = min_num_opts[im/3] + 1;
+      if(num_coins < min_num_opts[im]){
+        min_num_opts[im] = num_coins;
+        prev[im] = im/3;
+      }
     }
+    if(im % 2 == 0){
+      int num_coins = min_num_opts[im/2] + 1;
+      if(num_coins < min_num_opts[im]){
+        min_num_opts[im] = num_coins;
+        prev[im] = im/2;
+      }
+    }
+    int num_coins = min_num_opts[im-1] + 1;
+    if(num_coins < min_num_opts[im]){
+      min_num_opts[im] = num_coins;
+      prev[im] = im-1;
+    }  
+  }
+  int p = n;
+  while(p > 0){
+    sequence.push_back(p);
+    p = prev[p];
   }
   reverse(sequence.begin(), sequence.end());
   return sequence;
