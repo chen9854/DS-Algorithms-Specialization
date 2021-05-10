d372d5f1978a6ad5ed11ef81f8c09d450d90f615
ChangYuan Liiu <changyuan@ustc.edu>
2021-05-10 07:45:55 -0400

    Course 01 Week 05, 1/5

--- course1-algorithmic-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ course1-algorithmic-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-
+#include <map>
+#include <set>
+#include <limits>
 int get_change(int m) {
   //write your code here
-  return m / 4;
+  std::map<int,int> min_num_coins;
+  std::set<int> coins = {1,3,4};
+  min_num_coins[0] = 0;
+  for(int im = 1; im <= m; ++im)  {
+    min_num_coins[im] = std::numeric_limits<int>::max();
+    for(int c : coins){
+      if(im >= c){
+        int num_coins = min_num_coins[im - c] + 1;
+        if(num_coins < min_num_coins[im]){
+          min_num_coins[im] = num_coins;
+        }
+      }
+    }
+  }
+
+  return min_num_coins[m];
 }
 
 int main() {
