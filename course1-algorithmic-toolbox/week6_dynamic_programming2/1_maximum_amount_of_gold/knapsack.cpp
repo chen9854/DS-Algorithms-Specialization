d274d19c01aa0e5d69eba91f6f205c695fd080e1
ChangYuan Liiu <changyuan@ustc.edu>
2021-05-11 07:34:16 -0400

    Course 01, Week 06, 1/3

--- course1-algorithmic-toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ course1-algorithmic-toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -5,13 +5,34 @@ using std::vector;
 
 int optimal_weight(int W, const vector<int> &w) {
   //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
+  int value[10001][31];
+  int n = w.size();
+  for(int j = 0; j <= n; ++j){
+    value[0][j] = 0;
   }
-  return current_weight;
+  for(int v = 0; v <= W; ++v){
+    value[v][0] = 0;
+  }
+  for(int i = 1; i <= n; ++i){
+    for(int v = 1; v <= W; ++v){
+      value[v][i] = value[v][i-1];
+      if(w[i-1] <= v){
+        int val = value[v-w[i-1]][i-1] + w[i-1];
+        if(value[v][i] < val){
+          value[v][i] = val;
+        }
+      }
+    }
+
+  }
+  return value[W][n];
+  // int current_weight = 0;
+  // for (size_t i = 0; i < w.size(); ++i) {
+  //   if (current_weight + w[i] <= W) {
+  //     current_weight += w[i];
+  //   }
+  // }
+  // return current_weight;
 }
 
 int main() {
