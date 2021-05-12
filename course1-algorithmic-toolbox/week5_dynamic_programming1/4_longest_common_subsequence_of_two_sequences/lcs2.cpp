1edcb1102f1a1cab767e1dfa35954cc5a328dfbe
ChangYuan Liiu <changyuan@ustc.edu>
2021-05-12 13:17:23 -0400

    Course 01, Week 05, 4/5

--- course1-algorithmic-toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ course1-algorithmic-toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -3,18 +3,22 @@
 
 using std::vector;
 
-int lcs2(vector<int> &a, vector<int> &b) {
+int lcs2(vector<long long> &a, vector<long long> &b) {
   //write your code here
-  int value[101][101]{0};
+  int value[102][102]{0};
   int na = a.size();
   int nb = b.size();
   for(int i = 1; i <= na; ++i){
     for(int j = 1; j <= nb; ++j){
-      int val = value[i][j-1];
+      int val = std::max(value[i-1][j], value[i][j-1]);
       value[i][j] = val;
-      if(j<=i && a[i-1]==b[j-1]){
-        value[i][j] = std::max(val+1, value[i-1][j]);
+      if(a[i-1]==b[j-1]){
+        value[i][j] = std::max(val, value[i-1][j-1]+1);
       }
+      else{
+        value[i][j] = std::max(val, value[i-1][j-1]);
+      }
+
     }
   }
   return value[na][nb];
@@ -23,14 +27,14 @@ int lcs2(vector<int> &a, vector<int> &b) {
 int main() {
   size_t n;
   std::cin >> n;
-  vector<int> a(n);
+  vector<long long> a(n);
   for (size_t i = 0; i < n; i++) {
     std::cin >> a[i];
   }
 
   size_t m;
   std::cin >> m;
-  vector<int> b(m);
+  vector<long long> b(m);
   for (size_t i = 0; i < m; i++) {
     std::cin >> b[i];
   }
